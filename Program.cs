f0bf4b4a84b8f2cccb0aae4ea9eb7a5659fa295d
Linus Alpsten <linus.alpsten@telia.com>
2019-08-20 14:38:18 +0200

    update

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Advanced_Calculator
@@ -11,10 +12,78 @@ namespace Advanced_Calculator
         static void Main(string[] args)
         {
             string calculation = "((20+10)*4)+3*4+(12*29)";
+            Regex tester = new Regex(@"(\d+|[+\-*\/])");
+            string simplified = "12 + 23 * 34 - 45 / 56";
+            var matches = tester.Matches(simplified).Cast<Match>().Select(m => m.Value).ToArray();
+            foreach (var item in matches)
+            {
+                Console.WriteLine(item);
+            }
+            string[] operators = { "/", "*", "+", "-" };
+            while (matches.Length > 1)
+            {
+                bool matchesChanged = false;
+                for (int i = 0; i < matches.Length; i++)
+                {
+                    if (matches[i] == "*")
+                    {
+                        matches = matches.Take(i - 1).ToArray().Concat(matches.Skip(i + 2).ToArray().Prepend((Convert.ToDouble(matches[i - 1]) * Convert.ToDouble(matches[i + 1])).ToString())).ToArray();
+                        matchesChanged = true;
+                        break;
+                    }
+                }
+
+                if (matchesChanged)
+                {
+                    continue;
+                }
+                for (int i = 0; i < matches.Length; i++)
+                {
+                    if (matches[i] == "/")
+                    {
+                        matches = matches.Take(i - 1).ToArray().Concat(matches.Skip(i + 2).ToArray().Prepend((Convert.ToDouble(matches[i - 1]) / Convert.ToDouble(matches[i + 1])).ToString())).ToArray();
+                        matchesChanged = true;
+                        break;
+                    }
+                }
+
+                if (matchesChanged)
+                {
+                    continue;
+                }
+                for (int i = 0; i < matches.Length; i++)
+                {
+                    if (matches[i] == "+")
+                    {
+                        matches = matches.Take(i - 1).ToArray().Concat(matches.Skip(i + 2).ToArray().Prepend((Convert.ToDouble(matches[i - 1]) + Convert.ToDouble(matches[i + 1])).ToString())).ToArray();
+                        matchesChanged = true;
+                        break;
+                    }
+                }
+
+                if (matchesChanged)
+                {
+                    continue;
+                }
+                for (int i = 0; i < matches.Length; i++)
+                {
+                    if (matches[i] == "-")
+                    {
+                        matches = matches.Take(i - 1).ToArray().Concat(matches.Skip(i + 2).ToArray().Prepend((Convert.ToDouble(matches[i - 1]) - Convert.ToDouble(matches[i + 1])).ToString())).ToArray();
+                        matchesChanged = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        static float Calculate(string calculation)
+        {
             int lastindex = 0;
-            bool inParenthesis = false;
             int openParenthesisCount = 0;
-            List<string> segments = new List<string>();
+            string simplified = "";
+            float result = 0;
+
             for (int i = 0; i < calculation.Length; i++)
             {
                 char current = calculation[i];
@@ -22,13 +91,12 @@ namespace Advanced_Calculator
                 {
                     if (openParenthesisCount == 0)
                     {
-                        if (i != lastindex && i != lastindex+1)
+                        if (i != lastindex && i != lastindex + 1)
                         {
-                            segments.Add(calculation.Substring(lastindex, i - lastindex - 1));
+                            simplified.Concat(Calculate(calculation.Substring(lastindex + 1, i - lastindex - 1)).ToString());
                         }
                         lastindex = i;
                     }
-                    inParenthesis = true;
                     openParenthesisCount++;
                 }
                 else if (current == ')')
@@ -36,17 +104,15 @@ namespace Advanced_Calculator
                     openParenthesisCount--;
                     if (openParenthesisCount == 0)
                     {
-                        inParenthesis = false;
-                        segments.Add(calculation.Substring(lastindex, i - lastindex+1));
+                        simplified.Concat(calculation.Substring(lastindex, i - lastindex + 1));
                         lastindex = i;
                     }
                 }
             }
-            Console.WriteLine(calculation);
-            foreach (var item in segments)
-            {
-                Console.WriteLine(item);
-            }
+            Regex tester = new Regex(@"(\d+|[+\-*\/])");
+            var matches = tester.Matches(simplified).Cast<Match>().Select(m => m.Value).ToArray();
+
+            return result;
         }
     }
 }
