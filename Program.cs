bbc5be9ad27a889e4e408de2fe70cc1ae3586d57
Linus Alpsten <linus.alpsten@telia.com>
2019-08-26 14:18:25 +0200

    working with test string

--- Program.cs
+++ Program.cs
@@ -12,14 +12,55 @@ namespace Advanced_Calculator
         static void Main(string[] args)
         {
             string calculation = "((20+10)*4)+3*4+(12*29)";
-            Regex tester = new Regex(@"(\d+|[+\-*\/])");
-            string simplified = "12 + 23 * 34 - 45 / 56";
-            var matches = tester.Matches(simplified).Cast<Match>().Select(m => m.Value).ToArray();
-            foreach (var item in matches)
+            Console.WriteLine(Calculate(calculation).ToString());
+        }
+
+        static float Calculate(string calculation)
+        {
+            int lastindex = 0;
+            int openParenthesisCount = 0;
+            string simplified = "";
+            List<string> testlist = new List<string>();
+
+            for (int i = 0; i < calculation.Length; i++)
             {
-                Console.WriteLine(item);
+                char current = calculation[i];
+                if (current == '(')
+                {
+                    if (openParenthesisCount == 0)
+                    {
+                        if (i != lastindex && i != lastindex + 1)
+                        {
+                            simplified = new string(simplified.Concat(calculation.Substring(lastindex, i - lastindex)).ToArray());
+                            //testlist.Add(calculation.Substring(lastindex + 1, i - lastindex - 1));
+                        }
+                        lastindex = i;
+                    }
+                    openParenthesisCount++;
+                }
+                else if (current == ')')
+                {
+                    openParenthesisCount--;
+                    if (openParenthesisCount == 0)
+                    {
+                        simplified = new string(simplified.Concat(Calculate(calculation.Substring(lastindex + 1, i - lastindex - 1)).ToString()).ToArray());
+                        //testlist.Add(calculation.Substring(lastindex + 1, i - lastindex - 1));
+                        lastindex = i + 1;
+                    }
+                }
             }
-            string[] operators = { "/", "*", "+", "-" };
+
+            simplified = new string(simplified.Concat(calculation.Substring(lastindex, calculation.Length - lastindex)).ToArray());
+
+            Console.WriteLine(simplified);
+            var result = CalculateSimple(simplified);
+            return result;
+        }
+
+        static float CalculateSimple(string simplified)
+        {
+            Regex tester = new Regex(@"(\d+|[+\-*\/])");
+            var matches = tester.Matches(simplified).Cast<Match>().Select(m => m.Value).ToArray();
             while (matches.Length > 1)
             {
                 bool matchesChanged = false;
@@ -75,44 +116,7 @@ namespace Advanced_Calculator
                     }
                 }
             }
-        }
-
-        static float Calculate(string calculation)
-        {
-            int lastindex = 0;
-            int openParenthesisCount = 0;
-            string simplified = "";
-            float result = 0;
-
-            for (int i = 0; i < calculation.Length; i++)
-            {
-                char current = calculation[i];
-                if (current == '(')
-                {
-                    if (openParenthesisCount == 0)
-                    {
-                        if (i != lastindex && i != lastindex + 1)
-                        {
-                            simplified.Concat(Calculate(calculation.Substring(lastindex + 1, i - lastindex - 1)).ToString());
-                        }
-                        lastindex = i;
-                    }
-                    openParenthesisCount++;
-                }
-                else if (current == ')')
-                {
-                    openParenthesisCount--;
-                    if (openParenthesisCount == 0)
-                    {
-                        simplified.Concat(calculation.Substring(lastindex, i - lastindex + 1));
-                        lastindex = i;
-                    }
-                }
-            }
-            Regex tester = new Regex(@"(\d+|[+\-*\/])");
-            var matches = tester.Matches(simplified).Cast<Match>().Select(m => m.Value).ToArray();
-
-            return result;
+            return (float)Convert.ToDouble(matches[0]);
         }
     }
 }
